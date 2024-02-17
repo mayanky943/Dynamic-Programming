c979b093d14c2f6a5d92a80f87153fc4c1790618
Mayank Yadav <ymayank521@gmail.com>
2024-02-18 01:39:43 +0530

    added tabulation and space optimized code to frog jump

--- 3 Frog Jump.cpp
+++ 3 Frog Jump.cpp
@@ -27,12 +27,33 @@ int memoize(int n,vector<int> &heights,vector<int> &dp){
 	return dp[n]=min(jump1,jump2);
 }
 
-int tabulation(){
-
+int tabulation(int n,vector<int> &heights){
+	vector<int> dp(n,-1);
+	dp[0]=0;
+	for(int i=1;i<n;i++){
+		int jump1=dp[i-1]+abs(heights[i]-heights[i-1]);
+		int jump2=INT_MAX;
+		if(i>1)
+			jump2=dp[i-2]+abs(heights[i]-heights[i-2]);
+		dp[i]=min(jump1,jump2);
+	}
+	return dp[n-1];
 }
 
-int spaceOptimized(){
+int spaceOptimized(int n,vector<int> heights){
+	int prev=0;
+	int prev2=0;
+	for(int i=1;i<n;i++){
+		int jump1=prev+abs(heights[i]-heights[i-1]);
+		int jump2=INT_MAX;
+		if(i>1)
+			jump2=prev2+abs(heights[i]-heights[i-2]);
+		int curr=min(jump1,jump2);
 
+		prev2=prev;
+		prev=curr;
+	}
+	return prev;
 }
 
 int main(){
@@ -43,7 +64,9 @@ int main(){
 		cin>>heights[i];
 	}
 	// cout<<recur(n-1,heights);
-	vector<int> dp(n+1,-1);
-	cout<<memoize(n-1,heights,dp);
+	// vector<int> dp(n+1,-1);
+	// cout<<memoize(n-1,heights,dp);
+	// cout<<tabulation(n,heights);
+	cout<<spaceOptimized(n,heights);
 	return 0;
 }
